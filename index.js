4c9d290245cf9b1d5acd434f614aef662089067c
saika <93665867+saikadayo@users.noreply.github.com>
2025-01-20 22:02:16 -0600

    Defined End Points & Updated Documentation

--- index.js
+++ index.js
@@ -1,53 +1,138 @@
 const express = require("express");
+  bodyParser = require('body-parser'),
+  uuid = require('uuid');
+
 const app = express();
 const morgan = require("morgan");
 
-let topMovies = [{
+let movies = [{
     title: 'Parasite',
-    director: 'Bong Joon Ho'
+    director: {
+      name: 'Bong Joon Ho',
+      bio: 'He is a man who makes movies.',
+      birthYear: '1969'
+    },
+    genre: {
+      name: 'Thriller',
+      description: 'spooky movies'
+    }
   },
   {
     title: 'Past Lives',
-    director: 'Celine Song'
+    director: {
+      name: 'Celine Song',
+      bio: 'She makes romance movies.',
+      birthYear: '1988'
+    },
+    genre: {
+      name: 'Romance',
+      description: 'lovey dovey'
+    }
   },
   {
     title: 'Mother',
-    director: 'Bong Joon Ho'
+    director: {
+      name: 'Bong Joon Ho',
+      bio: 'He is a man who makes movies.',
+      birthYear: '1969'
+    },
+    genre: {
+      name: 'Horror',
+      description: 'terrifying things'
+    }
   },
   {
     title: 'Sympathy for Mr. Vengeance',
-    director: 'Park Chan-wook'
+    director: {
+      name: 'Park Chan-wook',
+      bio: 'He makes horror and romance films.',
+      birthYear: '1963'
+    },
+    genre: {
+      name: 'Horror',
+      description: 'terrifying things'
+    }
   },
   {
     title: 'Burning',
-    director: 'Lee Chang-Dong'
+    director: {
+      name: 'Lee Chang-Dong',
+      bio: 'He does suspense films.',
+      birthYear: '1954'
+    },
+    genre: {
+      name: 'Suspense',
+      description: 'puts you on the edge of your seat'
+    }
   },
   {
     title: 'The Handmaiden',
-    director: 'Park Chan-wook'
+    director: {
+      name: 'Park Chan-wook',
+      bio: 'He makes horror and romance films.',
+      birthYear: '1963'
+    },
+    genre: {
+      name: 'Romance',
+      description: 'lovey dovey'
+    }
   },
   {
     title: 'Castaway in the Moon',
-    director: 'Lee Hae-jun'
+    director: {
+      name: 'Lee Hae-jun',
+      bio: 'She makes lovey dovey movies.',
+      birthYear: '1973'
+    },
+    genre: {
+      name: 'Romance',
+      description: 'lovey dovey'
+    }
   },
   {
     title: '1987: When the Day Comes',
-    director: 'Jung Sung ho'
+    director: {
+      name: 'Jung Sung ho',
+      bio: 'He is a family man and makes family movies.',
+      birthYear: '1974'
+    },
+    genre: {
+      name: 'Drama',
+      description: 'all the tea'
+    }
   },
   {
     title: 'Marathon',
-    director: 'Yoon-Chul Jung'
+    director: {
+      name: 'Yoon-Chul Jung',
+      bio: 'He makes juicy drama films.',
+      birthYear: '1971'
+    },
+    genre: {
+      name: 'Drama',
+      description: 'all the tea'
+    }
   },
   {
     title: 'Moonlit Winter',
-    director: 'Dae Hyung Lim'
+    director: {
+      name: 'Dae Hyung Lim',
+      bio: 'He makes some swoon-worthy movies.',
+      birthYear: '1986'
+    },
+    genre: {
+      name: 'Romance',
+      description: 'lovey dovey'
+    }
   }
 ];
 
-app.use(morgan())
+let users= [];
+
+app.use(morgan());
+app.use(bodyParser.json());
 app.use(express.static('public'));
 
-// GET requests
 app.get('/', (req, res) => {
   res.send('Welcome to my Korean movie list!');
 });
@@ -58,8 +143,127 @@ app.get('/', (req, res) => {
 //   });
 // });
 
+// Get the list of all movies
 app.get('/movies', (req, res) => {
-  res.json(topMovies);
+  res.json(movies);
+});
+
+// Gets the data about a single movie
+app.get('/movies/:title', (req, res) => { 
+  const movie = movies.find((movie) => movie.title === req.params.title); 
+  
+  if (movie) { 
+    res.json(movie); 
+  } else { 
+    res.status(404).send('Movie not found.'); 
+  } 
+});
+
+// Get the data about a genre by name/title 
+app.get('/genre/:name', (req, res) => { 
+  const genre = movies.find((movie) => movie.genre.name.toLowerCase() === req.params.name.toLowerCase())?.genre; 
+  
+  if (genre) { 
+    res.json(genre); 
+  } else { 
+    res.status(400).send('Genre not found.'); 
+  } 
+}); 
+
+// Get the data about a director by name
+app.get('/director/:name', (req, res) => { 
+  const director = movies.find((movie) => movie.director.name.toLowerCase() === req.params.name.toLowerCase())?.director; 
+  
+  if (director) { 
+    res.json(director); 
+  } else { 
+    res.status(400).send('Director not found.'); 
+  } 
+});
+
+// Add a new movie to our list of movies
+app.post('/movies', (req, res) => {
+  let newMovie = req.body;
+
+  if (!newMovie.title) {
+    const message = 'Missing title in request body';
+    res.status(400).send(message);
+  } else {
+    newMovie.id = uuid.v4();
+    movies.push(newMovie);
+    res.status(201).send(newMovie);
+  }
+});
+
+// Delete a movie from our list by ID
+app.delete('/movies/:id', (req, res) => {
+  let movie = movies.find((movie) => { return movie.id === req.params.id });
+
+  if (movie) {
+    movies = movies.filter((obj) => { return obj.id !== req.params.id });
+    res.status(201).send('Movie ' + req.params.id + ' was deleted.');
+  }
+});
+
+// Allow new users to register 
+app.post('/users', (req, res) => { 
+  let newUser = req.body; 
+  if (!newUser.username) { 
+    const message = 'Missing username in request body'; 
+    res.status(400).send(message); 
+  } else { 
+    newUser.id = uuid.v4(); 
+    users.push(newUser); 
+    res.status(201).send(newUser); 
+  } 
+}); 
+  
+// Allow users to update username
+app.put('/users/:id', (req, res) => { 
+  let user = users.find((user) => user.id === req.params.id); 
+  if (user) { 
+    user.username = req.body.username; 
+    res.status(200).send(user); 
+  } else { 
+    res.status(404).send('User not found.'); 
+  }
+}); 
+
+// Allow users to add movie to their favorite list
+app.post('/users/:id/movies/:movieTitle', (req, res) => { 
+  let user = users.find((user) => user.id === req.params.id); 
+  let movie = movies.find((movie) => movie.title === req.params.movieTitle); 
+  if (user && movie) { 
+    if (!user.favorites) { 
+      user.favorites = []; 
+    } 
+    user.favorites.push(movie.title); 
+    res.status(200).send(`Movie '${movie.title}' has been added to your favorites!`); 
+  } else { 
+    res.status(404).send('User or movie not found.'); 
+  } 
+}); 
+
+// Allow users to remove a movie from their favorite list
+app.delete('/users/:id/movies/:movieTitle', (req, res) => { 
+  let user = users.find((user) => user.id === req.params.id); 
+  if (user && user.favorites) { 
+    user.favorites = user.favorites.filter((title) => title !== req.params.movieTitle); 
+    res.status(200).send(`Movie '${req.params.movieTitle}' has been removed from your favorites!`); 
+  } else { 
+    res.status(404).send('User not found.'); 
+  }
+}); 
+
+// Allow existing users to deregister
+app.delete('/users/:id', (req, res) => { 
+  let user = users.find((user) => user.id === req.params.id); 
+  if (user) { 
+    users = users.filter((obj) => obj.id !== req.params.id); 
+    res.status(200).send(`User with ID ${req.params.id} has been deregistered.`); 
+  } else { 
+    res.status(404).send('User not found.'); 
+}
 });
 
 // let myLogger = (req, res, next) => {
