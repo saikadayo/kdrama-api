8145e3f9d98afb62e83d45597c740e342c72b73c
saika <93665867+saikadayo@users.noreply.github.com>
2026-02-20 02:58:24 -0600

    Implemented hashing and server-side input validation
    Updated registration, login, and user update endpoints to ensure authentication integrity and input validation.

--- index.js
+++ index.js
@@ -10,8 +10,25 @@ const express = require("express");
   bodyParser = require('body-parser'),
   uuid = require('uuid');
 
+const { check, validationResult } = require('express-validator');
+
 const app = express();
 const morgan = require("morgan");
+
+const cors = require('cors');
+
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
+      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+      return callback(new Error(message ), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 let auth = require('./auth')(app);
 const passport = require('passport');
@@ -218,26 +235,45 @@ app.delete('/movies/:id', passport.authenticate('jwt', { session: false }), (req
   }
 });
 
-// Allow new users to register 
-app.post('/users', async (req, res) => {
-  try {
-    const existingUser = await Users.findOne({ Username: req.body.Username });
-    if (existingUser) {
-      return res.status(400).send(req.body.Username + ' already exists');
-    } else {
-      const newUser = await Users.create({
-        Username: req.body.Username,
-        Password: req.body.Password,
-        Email: req.body.Email,
-        Birthday: req.body.Birthday
-      });
-      res.status(201).json(newUser);
+app.post('/users',
+  [
+    check('Username', 'Username must be at least 5 characters').isLength({min: 5}),
+    check('Username', 'Username contains non-alphanumeric characters that are not allowed.').isAlphanumeric(),
+    check('Password', 'Password is required').not().isEmpty(),
+    check('Email', 'Email does not appear to be valid').isEmail()
+  ], async (req, res) => {
+
+    let errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
     }
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Error: ' + error);
-  }
-});
+
+    let hashedPassword = Users.hashPassword(req.body.Password);
+    await Users.findOne({ Username: req.body.Username })
+      .then((user) => {
+        if (user) {
+          return res.status(400).send(req.body.Username + ' already exists');
+        } else {
+          Users
+            .create({
+              Username: req.body.Username,
+              Password: hashedPassword,
+              Email: req.body.Email,
+              Birthday: req.body.Birthday
+            })
+            .then((user) => { res.status(201).json(user) })
+            .catch((error) => {
+              console.error(error);
+              res.status(500).send('Error: ' + error);
+            });
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+        res.status(500).send('Error: ' + error);
+      });
+  });
 
 // Get all users
 app.get('/users', passport.authenticate('jwt', { session: false }), async (req, res) => {
@@ -265,63 +301,80 @@ app.get('/users/:Username', passport.authenticate('jwt', { session: false }), as
 
 // Allow users to update username
 app.put('/users/:Username', passport.authenticate('jwt', { session: false }), async (req, res) => {
-  try {
-    if (req.user.Username !== req.params.Username) return res.status(400).send('Permission denied');
-    const updatedUser = await Users.findOneAndUpdate(
-      { Username: req.params.Username },
-      { $set: { Username: req.body.Username, Password: req.body.Password, Email: req.body.Email, Birthday: req.body.Birthday } },
-      { new: true }
-    );
+  if (req.user.Username !== req.params.Username) return res.status(400).send('Permission denied');
+
+  let updateData = {
+    Username: req.body.Username,
+    Email: req.body.Email,
+    Birthday: req.body.Birthday
+  };
+
+  if (req.body.Password) {
+    updateData.Password = Users.hashPassword(req.body.Password);
+  }
+
+  Object.keys(updateData).forEach((key) => updateData[key] === undefined && delete updateData[key]);
+
+  await Users.findOneAndUpdate({ Username: req.params.Username }, {
+    $set: updateData
+  },
+  { new: true })
+  .then((updatedUser) => {
     res.json(updatedUser);
-  } catch (err) {
+  })
+  .catch((err) => {
     console.error(err);
     res.status(500).send('Error: ' + err);
-  }
+  })
 });
 
 // Allow users to add movie to their favorite list
 app.post('/users/:Username/movies/:MovieID', passport.authenticate('jwt', { session: false }), async (req, res) => {
-  try {
-    if (req.user.Username !== req.params.Username) return res.status(400).send('Permission denied');
-    const updatedUser = await Users.findOneAndUpdate(
-      { Username: req.params.Username },
-      { $push: { FavoriteMovies: req.params.MovieID } },
-      { new: true }
-    );
+  if (req.user.Username !== req.params.Username) return res.status(400).send('Permission denied');
+  await Users.findOneAndUpdate({ Username: req.params.Username }, {
+    $push: { FavoriteMovies: req.params.MovieID }
+  },
+  { new: true })
+  .then((updatedUser) => {
     res.json(updatedUser);
-  } catch (err) {
+  })
+  .catch((err) => {
     console.error(err);
     res.status(500).send('Error: ' + err);
-  }
+  });
 });
 
 // Allow users to remove a movie from their favorite list
 app.delete('/users/:Username/movies/:MovieID', passport.authenticate('jwt', { session: false }), async (req, res) => {
-  try {
-    if (req.user.Username !== req.params.Username) return res.status(400).send('Permission denied');
-    const updatedUser = await Users.findOneAndUpdate(
-      { Username: req.params.Username },
-      { $pull: { FavoriteMovies: req.params.MovieID } },
-      { new: true }
-    );
+  if (req.user.Username !== req.params.Username) return res.status(400).send('Permission denied');
+  await Users.findOneAndUpdate({ Username: req.params.Username }, {
+    $pull: { FavoriteMovies: req.params.MovieID }
+  },
+  { new: true })
+  .then((updatedUser) => {
     res.json(updatedUser);
-  } catch (err) {
+  })
+  .catch((err) => {
     console.error(err);
     res.status(500).send('Error: ' + err);
-  }
+  });
 });
 
 // Allow existing users to deregister
 app.delete('/users/:Username', passport.authenticate('jwt', { session: false }), async (req, res) => {
-  try {
-    if (req.user.Username !== req.params.Username) return res.status(400).send('Permission denied');
-    const user = await Users.findOneAndDelete({ Username: req.params.Username });
-    if (!user) return res.status(400).send(req.params.Username + ' was not found');
-    res.status(200).send(req.params.Username + ' was deleted.');
-  } catch (err) {
-    console.error(err);
-    res.status(500).send('Error: ' + err);
-  }
+  if (req.user.Username !== req.params.Username) return res.status(400).send('Permission denied');
+  await Users.findOneAndDelete({ Username: req.params.Username })
+    .then((user) => {
+      if (!user) {
+        res.status(400).send(req.params.Username + ' was not found');
+      } else {
+        res.status(200).send(req.params.Username + ' was deleted.');
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
 });
 
 // let myLogger = (req, res, next) => {
@@ -341,7 +394,6 @@ app.get('/secreturl', passport.authenticate('jwt', { session: false }), (req, re
   let responseText = 'This is a secret url with super top-secret content.';
   responseText += '<small>Requested at: ' + req.requestTime + '</small>';
   res.send(responseText);
-
 });
 
 app.use((err, req, res, next) => {
--- passport.js
+++ passport.js
@@ -23,6 +23,10 @@ passport.use(
             message: 'Incorrect username or password.',
           });
         }
+        if (!user.validatePassword(password)) {
+          console.log('incorrect password');
+          return callback(null, false, { message: 'Incorrect password.' });
+        }
         console.log('finished');
         return callback(null, user);
       })
@@ -35,7 +39,6 @@ passport.use(
     }
   )
 );
-
 
 passport.use(new JWTStrategy({
   jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
